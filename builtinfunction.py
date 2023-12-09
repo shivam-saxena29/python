4589ed7155f784f52356ddc971e8027c0070e9fb
shivam-saxena29 <153342617+shivam-saxena29@users.noreply.github.com>
2023-12-09 09:45:44 +0530

    Add files via upload

---
+++ builtinfunction.py
@@ -0,0 +1,38 @@
+#1. ABSLOUTE IN BUITL FUNCTION
+
+# a=int(input("enter the value of a= "))
+# print("absloute value of a= ",abs(a))
+# b=float(input("enter the value of b= "))
+# print("absloute value of b= ",abs(b))
+
+#2. ALL BUILT IN FUNCTION
+
+#all values are true
+# k=[1,2,3,4]
+# print(all(k))
+
+# #all values are false
+# d=[0,False]
+# print(all(d))
+
+#3. EXEC BUILT IN FUNCTION
+
+# x=8
+# exec('print(x==8)')
+# exec('print(x+4)')
+
+
+#4. SUM() BUILT IN FUNCTION
+# s=sum([1,2,4])
+# print(s)
+# s=sum([1,2,4],10)
+# print(s)
+
+# ANY() BUILT IN FUNCTION
+
+i=[4,3,2,0]
+print(any(i)) 
+i=[0,False]
+print(any(i))
+i=[0,False,5]
+print(any(i))
